f4309880c466ebbe39d6e717468dbc5977c2b1af
ShankS3 <shashank.surwase@gmail.com>
2020-09-13 00:00:29 +0530

    Added application constants

---
+++ src/constants/applicationConstants.js
@@ -0,0 +1,11 @@
+//Status codes
+export const STATUS_SUCCESS = 200;
+export const STATUS_UNAUTHORIZED = 401;
+export const INTERNAL_SERVER_ERROR = 500;
+export const BAD_REQUEST = 400;
+export const FORBIDDEN = 403;
+export const NOT_FOUND = 404;
+
+//Other constants
+export const RADIX = 10;
+export const BASE_URL = '';
